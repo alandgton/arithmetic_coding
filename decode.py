5a38f49711057dc67dc98bc03903fa32ae162676
alanton <alandgton@gmail.com>
2023-08-25 21:44:44 -0400

    decode logic -- need to figure out python float precision bug

--- decode.py
+++ decode.py
@@ -1,2 +1,31 @@
+from sys import argv
+from string import printable
+
+EOF = r"\EOF"
+
+def decode(num):
+    """
+    0.16969768002839405 -> "hello"
+    """
+    result = []
+    lower, upper = 0.0, 1.0
+    freq = {c: 1 for c in printable}
+    freq[EOF] = 1
+    while True:
+        offset = lower
+        for i, cc in enumerate(freq):
+            slice_size = freq[cc]*(upper-lower)/sum(freq.values())
+            if offset + slice_size > num:
+                result.append(cc)
+                freq[cc] += 1
+                lower, upper = offset, offset + slice_size
+                break
+            else:
+                offset += slice_size
+        if result[-1] == EOF:
+            result.pop()
+            break
+    return "".join(result)
+
 if __name__ == '__main__':
-    print("hello")
+    print(decode(float(argv[1])))
